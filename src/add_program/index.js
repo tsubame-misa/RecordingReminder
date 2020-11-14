c5be7951de24dac5e1a39dfd0516e25c8a04210f
tsubame_misa <chmi19013@g.nihon-u.ac.jp>
2020-11-14 23:33:58 +0900

    push data

--- src/add_program/index.js
+++ src/add_program/index.js
@@ -17,18 +17,76 @@ import {
   IonDatetime,
   IonInput,
   IonItemDivider,
+  IonList,
 } from "@ionic/react";
-import { add, trash } from "ionicons/icons";
+import { add, contractOutline, trash } from "ionicons/icons";
 import { useState } from "react";
 
 const Addprogram = () => {
-  const [showAlert, setShowAlert] = useState(false);
-  const [showAlert5, setShowAlert5] = useState(false);
+  const [programName, setProgramName] = useState();
   const [text, setText] = useState();
-  const [text2, setText2] = useState();
-  const [hairColor, setHairColor] = useState();
+  const [selectedChannel, setSelectedChannel] = useState(null);
   const [selectedDate, setSelectedDate] = useState();
-  const [selectedDate2, setSelectedDate2] = useState();
+  const [perfTime, setPerfTime] = useState();
+  const [artist, setArtist] = useState();
+
+  const channel = [
+    { name: "NHK総合" },
+    { name: "Eテレ" },
+    { name: "日テレ" },
+    { name: "テレビ東京" },
+    { name: "TBS" },
+    { name: "テレビ朝日" },
+    { name: "フジテレビ" },
+    { name: "東京MX" },
+  ];
+
+  const artists = [
+    { name: "山田涼介" },
+    { name: "知念侑李" },
+    { name: "中島裕翔" },
+    { name: "岡本圭人" },
+    { name: "有岡大貴" },
+    { name: "伊野尾慧" },
+    { name: "高木雄也" },
+    { name: "八乙女光" },
+    { name: "藪宏太" },
+  ];
+
+  const sendData = () => {
+    if (
+      selectedChannel === null ||
+      selectedDate === null ||
+      programName === null ||
+      artist === null
+    ) {
+      alert("記入漏れがあります");
+    }
+
+    const data = {
+      channel: selectedChannel,
+      date: selectedDate,
+      name: programName,
+      artist: artist,
+      perfTime: perfTime,
+      comment: text,
+    };
+
+    let datalist = JSON.parse(localStorage.getItem("data"));
+    if (datalist === null) {
+      datalist = [data];
+    } else {
+      datalist.push(data);
+    }
+    localStorage.setItem("data", JSON.stringify(datalist));
+
+    setSelectedChannel(null);
+    setSelectedDate(null);
+    setProgramName(null);
+    setArtist(null);
+    setPerfTime(null);
+    setText(null);
+  };
 
   return (
     <IonPage>
@@ -44,15 +102,16 @@ const Addprogram = () => {
         <IonItem>
           <IonLabel>チャンネル名</IonLabel>
           <IonSelect
-            value={hairColor}
+            value={selectedChannel}
             okText="Okay"
             cancelText="Dismiss"
-            onIonChange={(e) => setHairColor(e.detail.value)}
+            onIonChange={(e) => {
+              setSelectedChannel(e.detail.value);
+            }}
           >
-            <IonSelectOption value="brown">Brown</IonSelectOption>
-            <IonSelectOption value="blonde">Blonde</IonSelectOption>
-            <IonSelectOption value="black">Black</IonSelectOption>
-            <IonSelectOption value="red">Red</IonSelectOption>
+            {channel.map((t) => {
+              return <IonSelectOption value={t.name}>{t.name}</IonSelectOption>;
+            })}
           </IonSelect>
         </IonItem>
         <IonItem>
@@ -66,28 +125,60 @@ const Addprogram = () => {
         </IonItem>
         <IonItem>
           <IonInput
-            value={text}
+            value={programName}
             placeholder="番組名"
-            onIonChange={(e) => setText(e.detail.value)}
+            onIonChange={(e) => setProgramName(e.detail.value)}
           ></IonInput>
         </IonItem>
+
+        <IonItem>
+          <IonLabel>アーティスト</IonLabel>
+          <IonSelect
+            value={artist}
+            okText="Okay"
+            cancelText="Dismiss"
+            onIonChange={(e) => {
+              setArtist(e.detail.value);
+            }}
+          >
+            {artists.map((a) => {
+              return <IonSelectOption value={a.name}>{a.name}</IonSelectOption>;
+            })}
+          </IonSelect>
+        </IonItem>
+
         <IonItem>
           <IonLabel>アーティスト出演時刻</IonLabel>
           {/*別の形式がいいかもしれない */}
           <IonDatetime
             displayFormat="HH:mm ~ HH:mm"
             placeholder="Select Date"
-            value={selectedDate2}
-            onIonChange={(e) => setSelectedDate2(e.detail.value)}
+            value={perfTime}
+            onIonChange={(e) => setPerfTime(e.detail.value)}
           ></IonDatetime>
         </IonItem>
         <IonItem>
           <IonTextarea
-            placeholder=""
-            value={text2}
-            onIonChange={(e) => setText2(e.detail.value)}
+            placeholder="その他・コメント"
+            value={text}
+            onIonChange={(e) => setText(e.detail.value)}
           ></IonTextarea>
         </IonItem>
+        <IonButton
+          expand="full"
+          onClick={() => {
+            sendData(
+              selectedChannel,
+              selectedDate,
+              programName,
+              artist,
+              perfTime,
+              text
+            );
+          }}
+        >
+          登録
+        </IonButton>
       </IonContent>
     </IonPage>
   );
